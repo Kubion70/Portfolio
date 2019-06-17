fb88997233f66cf4f76438d1f03cbda3809471f2
Kubion <jakub98kowalski@gmail.com>
2019-06-17 22:02:52 +0200

    hot reload + node fixes

--- Portfolio.ClientApp/app.tsx
+++ Portfolio.ClientApp/app.tsx
@@ -1,14 +1,20 @@
-﻿declare var require: any
-
-var React = require('react');
-var ReactDOM = require('react-dom');
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { MyButton } from './components/MyButton';
+declare let module: any;
 
 export class Hello extends React.Component {
     render() {
         return (
-            <h1>Welcome to React!!</h1>
+            <React.Fragment>
+                <h1>Welcome tffdddo123456 React!!</h1>
+            </React.Fragment>
         );
     }
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
+
+if (module.hot) {
+    module.hot.accept();
+}
