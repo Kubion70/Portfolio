9318356035cb91424ab19a56c02f7943ede93235
Kubion <jakub98kowalski@gmail.com>
2019-07-15 21:50:27 +0200

    Add DbUp and Swagger

---
+++ Portfolio.Database/Portfolio.Database.Upgrader/DbUpgrader.cs
@@ -0,0 +1,42 @@
+﻿using DbUp;
+using System;
+using System.Reflection;
+
+namespace Portfolio.Database.Upgrader
+{
+    public class DbUpgrader
+    {
+        private string ConnectionString { get; }
+
+        public DbUpgrader(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public void Upgrade(bool withScripts = true, bool withSeeds = true, bool withFakes = false)
+        {
+            EnsureDatabase.For.SqlDatabase(ConnectionString);
+
+            var upgradeBuilder = DeployChanges.To
+                .SqlDatabase(ConnectionString)
+                .WithTransaction()
+                .LogToConsole();
+
+            if (withScripts)
+                upgradeBuilder.WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), s => s.StartsWith("Scripts"));
+            if (withSeeds)
+                upgradeBuilder.WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), s => s.StartsWith("Seeds"));
+            if (withFakes)
+                upgradeBuilder.WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), s => s.StartsWith("Fakes"));
+
+            var result = upgradeBuilder.Build().PerformUpgrade();
+
+            if (!result.Successful)
+                throw result.Error;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Database upgrade success!");
+            Console.ResetColor();
+        }
+    }
+}
--- Portfolio.Web/Startup.cs
+++ Portfolio.Web/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Portfolio.Database.Upgrader;
 using Portfolio.Web.Configurations;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Portfolio.Web
 {
@@ -22,7 +25,24 @@ namespace Portfolio.Web
             Configuration.Bind("ClientApp", clientAppConfiguration);
             services.AddSingleton(clientAppConfiguration);
 
-            services.AddMvc();
+            // Cors config
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.WithOrigins(clientAppConfiguration.RedirectUrl)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Portfolio API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -30,6 +50,29 @@ namespace Portfolio.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors();
+
+            // Database upgrade
+            var dbUpgrader = new DbUpgrader(Configuration["ConnectionString"]);
+            if (env.IsDevelopment())
+            {
+                dbUpgrader.Upgrade(withFakes: true);
+            }
+            else
+            {
+                dbUpgrader.Upgrade();
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portfolio API");
+                    c.RoutePrefix = "api";
+                });
             }
 
             app.UseMvcWithDefaultRoute();
